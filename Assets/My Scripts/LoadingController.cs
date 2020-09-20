3e4f8a7353d2261dbb19d3f9c0b370cf925ade06
Quattro <tylercook142@gmail.com>
2020-09-20 20:29:28 +0700

    Added primitive LOADER+

--- Assets/My Scripts/LoadingController.cs
+++ Assets/My Scripts/LoadingController.cs
@@ -1,11 +1,37 @@
 ﻿using UnityEngine.SceneManagement;
+using System.Collections;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class LoadingController : MonoBehaviour
 {
+    [Header("Загружаемая сцена")]
+    [SerializeField] private int sceneNumber;
+    [Header("Остальные объекты")]
+    [SerializeField] Image imageLoading;
+    [SerializeField] Text textLoadPercent;
+
     private void Start()
     {
         PlayerData.Init();
-        SceneManager.LoadScene(1);
+        StartCoroutine(AsyncLoad());
+    }
+
+    IEnumerator AsyncLoad()
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneNumber);
+
+        operation.allowSceneActivation = false;
+        while (!operation.isDone)
+        {
+            float progress = operation.progress / 0.9f;
+            imageLoading.fillAmount = progress;
+            textLoadPercent.text = string.Format("{0:0}%", progress * 100);
+            if (Input.anyKeyDown)
+            {
+                operation.allowSceneActivation = true;
+            }
+            yield return null;
+        }
     }
 }
